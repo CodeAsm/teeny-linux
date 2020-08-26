a9538435a0e8b841f0b9ca499b44d061072ece36
codeasm <nico.vijlbrief@outlook.com>
2020-08-27 00:39:55 +0200

    tcc compiles and gets inside the env again. no path include yet tho

--- tcc.sh
+++ tcc.sh
@@ -4,7 +4,8 @@ TARGET=$HOME/Projects/Emulation/Linux/bin/build
                                
 # a bunch of helpfull functions
 #----------------------------------------------------------------------
-./musl.sh
+./musl.sh -d
+./musl.sh 
 ## The Linux headers might be needed for tcc
 cd /home/codeasm/Projects/Emulation/Linux/bin/linux-5.8.4/
 #make headers_install INSTALL_HDR_PATH=$TARGET
@@ -12,20 +13,20 @@ cd ..
 
 rm -rf tcc-0.9.27/
 
-#wget -c https://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27.tar.bz2
-#tar -xvf tcc-0.9.27.tar.bz2
-#cd tcc-0.9.27/
-#./configure --enable-static --config-musl --cc=musl-gcc --prefix=/usr
-#make CC="musl-gcc -static" -j8
-#make test
-#make install DESTDIR=$TARGET
+wget -c https://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27.tar.bz2
+tar -xvf tcc-0.9.27.tar.bz2
+cd tcc-0.9.27/
+./configure --enable-static --config-musl --cc=musl-gcc --prefix=/usr
+make CC="musl-gcc -static" -j8
+make test
+make install DESTDIR=$TARGET
 
-#wget -c https://ftp.gnu.org/gnu/make/make-4.3.tar.gz
-#tar -xvf make-4.3.tar.gz
-#cd make-4.3
-#CC="musl-gcc -static" ./configure --prefix=/usr --without-guile && make
-#make install DESTDIR=$TARGET
-#cd ..
+wget -c https://ftp.gnu.org/gnu/make/make-4.3.tar.gz
+tar -xvf make-4.3.tar.gz
+cd make-4.3
+CC="musl-gcc -static" ./configure --prefix=/usr --without-guile && make
+make install DESTDIR=$TARGET
+cd ..
 
 # make headers_install INSTALL_HDR_PATH=/home/codeasm/projects/Emulation/Linux/bin/build 
 #DESTDIR=
