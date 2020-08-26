5352150881261e0f7d90ed08f6a2ef9ba523ce9e
codeasm <nico.vijlbrief@outlook.com>
2020-08-27 00:33:13 +0200

    tcc tests for main folder

--- tcc.sh
+++ tcc.sh
@@ -1,37 +1,31 @@
 #!/bin/sh
-wget -c https://musl.cc/x86_64-linux-musl-native.tgz
-tar -xvf x86_64-linux-musl-native.tgz
-#cp -r x86_64-linux-musl-native/. build/
-
-
-# the following is a result of me trying to get TCC to compile and work
-# TCC compiles, staticly and runs... but I yet have to figure out how to run
-# TCC with crt and stdio.h working (I can get it to print its missing printf.h :P
-
-## Only these files dint seem to work
-#cp -r x86_64-linux-musl-native/lib/crti.o build/lib/crti.o
-#cp -r x86_64-linux-musl-native/lib/crt1.o build/lib/crt1.o
-#cp -r x86_64-linux-musl-native/lib/crtn.o build/lib/crtn.o
-
-## These folders are needed?
-cp -r x86_64-linux-musl-native/lib build
-cp -r x86_64-linux-musl-native/include build
-
+TARGET=$HOME/Projects/Emulation/Linux/bin/build    
+                                 #location for the build, change this for your location
+                               
+# a bunch of helpfull functions
+#----------------------------------------------------------------------
+./musl.sh
 ## The Linux headers might be needed for tcc
 cd /home/codeasm/Projects/Emulation/Linux/bin/linux-5.8.4/
-make headers_install INSTALL_HDR_PATH=/home/codeasm/projects/Emulation/Linux/bin/build
+#make headers_install INSTALL_HDR_PATH=$TARGET
 cd ..
 
 rm -rf tcc-0.9.27/
 
-wget -c https://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27.tar.bz2
-tar -xvf tcc-0.9.27.tar.bz2
-cd tcc-0.9.27/
-./configure --enable-static --config-musl --cc=musl-gcc --prefix=/usr
-make CC="musl-gcc -static" -j8
-make test
-make install DESTDIR=/home/codeasm/Projects/Emulation/Linux/bin/build
+#wget -c https://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27.tar.bz2
+#tar -xvf tcc-0.9.27.tar.bz2
+#cd tcc-0.9.27/
+#./configure --enable-static --config-musl --cc=musl-gcc --prefix=/usr
+#make CC="musl-gcc -static" -j8
+#make test
+#make install DESTDIR=$TARGET
 
+#wget -c https://ftp.gnu.org/gnu/make/make-4.3.tar.gz
+#tar -xvf make-4.3.tar.gz
+#cd make-4.3
+#CC="musl-gcc -static" ./configure --prefix=/usr --without-guile && make
+#make install DESTDIR=$TARGET
+#cd ..
 
 # make headers_install INSTALL_HDR_PATH=/home/codeasm/projects/Emulation/Linux/bin/build 
 #DESTDIR=
