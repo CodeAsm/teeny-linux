834c827774c08b13b783b3e747a50630f987aaf4
codeasm <nico.vijlbrief@outlook.com>
2021-02-28 17:00:35 +0100

    Cleaned up some mkiso script issues

--- mkiso.sh
+++ mkiso.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 TOP=$HOME/Projects/Emulation/Linux/bin  #location for the build, change this for your location
+ARCH="x86_64"                   #default arch
+ARC="x86"                       #short arch (can I use grep for this?)
+KERNEL="5.11.2"
+## The following worked almost, just I dint use the right format for this
+#while read line; do
+#    [[ $line =~ KERNEL= ]] && declare "$line" && break
+#done < ./build.sh  
 
+cd $TOP
+
+#First some sanity checks -------------------------------------------------
 if ! command -v grub-mkrescue &> /dev/null
 then
     echo "grub-mkrescue could not be found"
@@ -13,8 +23,17 @@ then
     exit
 fi
 
+if [ ! -f $TOP/obj/initramfs-busybox-$ARC.cpio.gz ]; then
+    echo "initramfs could not be found, run build first!"
+    exit
+fi
 
-cd $TOP
+    if [ ! -f $TOP/obj/linux-$ARC/arch/$ARC/boot/bzImage ]; then
+    echo "Linux kernel could not be found, run build first!"
+    exit
+fi
+
+# Everything seems to be in place, lets rock and roll baby -----------------
 mkdir iso
 cd iso
 mkdir boot
@@ -26,19 +45,19 @@ cat << EOF> grub.cfg
 set menu_color_normal=white/black
 set menu_color_highlight=light-blue/black
 
-menuentry "TeenyLinux" {
+menuentry "TeenyLinux $KERNEL" {
 	set gfxpayload=keep
 	linux	/boot/vmlinuz console=tty0 
 	initrd	/boot/initramfs.cpio.gz
 }
 
-menuentry "TeenyLinux Serial" {
+menuentry "TeenyLinux $KERNEL Serial" {
 	linux	/boot/vmlinuz console=ttyS0 
 	initrd	/boot/initramfs.cpio.gz
 }
 EOF
 cd $TOP
-cp obj/linux-x86/arch/x86_64/boot/bzImage iso/boot/vmlinuz
+cp obj/linux-x86/arch/x86/boot/bzImage iso/boot/vmlinuz
 cp obj/initramfs-busybox-x86.cpio.gz iso/boot/initramfs.cpio.gz
 grub-mkrescue --xorriso=/bin/xorriso -o boot.iso iso/ 
 qemu-system-x86_64 -cdrom boot.iso
