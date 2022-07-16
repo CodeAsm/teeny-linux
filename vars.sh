61ae7d6b42988bdbcbbfe39b5235e98bc4355f00
CodeAsm <nico.vijlbrief@outlook.com>
2022-07-16 22:58:39 +0200

    Kernel 5.18.12

--- vars.sh
+++ vars.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TOP=$HOME/Projects/Emulation/Linux/bin             #location for the build, change this for your location
-KERNEL="5.18.11"                 #Kernel release number. (or see cli options)
+KERNEL="5.18.12"                 #Kernel release number. (or see cli options)
 V="${KERNEL:0:1}"               #Kernel version for folder (probably breaks when 10 or larger)
 KTYPE="xz"                      #gz used by RC, xz by stable releases, but should work.
                                 #if posible, I would prever xz for its size and decompress seed
@@ -16,9 +16,9 @@ GATEWAY="192.168.66.1"          #default gateway to be used
 DNS="1.1.1.1"                   #default DNS, use 8.8.8.8 if you want silly google
 HOSTNAME="TeenyQemuBox"         #hostname
 MODULEURL=$TOP/../teeny-linux/modules/        #modprobe url
-LOGINREQUIRED="/bin/login"      #replace with /bin/sh for no login required, /bin/login needed else 
+LOGINREQUIRED="/bin/login"      #replace with /bin/sh for no login required, /bin/login needed else
                                 #seems one can simply Ctrl+C out of login tho
-#Host specific variables, change if poormans host                               
+				#Host specific variables, change if poormans host
 CORECOUNT=$(nproc)              #$(nproc) or change to a number like 1 or 8 depending on the core count to use
 RAM="1G"                        #1G minimal for qemu , 2G or more recommended. (used only for qemu invocation)
 
